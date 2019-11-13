81ede0b578d443d778e884c2f82094ab3518df89
Mark Gold <mark.gold@ezoka.net>
2019-11-13 13:14:14 +0000

    Fixed typo in auto_update.bat

--- Auto_Update.bat
+++ Auto_Update.bat
@@ -1,5 +1,5 @@
 @echo on
 cd C:\ProgramData\sysmon\
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/markxgold/sysmon-modular/master/sysmonconfig.xml','C:\ProgramData\sysmon\sysmonconfig].xml')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/markxgold/sysmon-modular/master/sysmonconfig.xml','C:\ProgramData\sysmon\sysmonconfig.xml')"
 sysmon64 -c sysmonconfig.xml
 exit
